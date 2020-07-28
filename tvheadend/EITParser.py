386fafd05b343b607afe183169057282208065bc
charrus <charlie@rusbridger.com>
2020-07-28 22:23:32 +0100

    Fixes

--- tvheadend/EITParser.py
+++ tvheadend/EITParser.py
@@ -4,49 +4,62 @@ import re
 
 class EITParser:
 
+    re_sub = re.compile(r'.*"epggrab" subscribing to mux \"([^"]+)"')
+    re_epggrab = re.compile(r'.*epggrab: (.+) - (.*)$')
+    re_unsub = re.compile(r'.*subscription: [0-9a-fA-F]{4}: "epggrab" unsubscribing')
+
     def __init__(self):
         self.muxes = {}
         self.eits = {}
     
     def parse(self, file):
         current_mux = None
-        current_eit = None
 
-        re_sub = re.compile(r'.*"epggrab" subscribing to mux \"([^"]+)"')
-        re_epggrab = re.compile(r'.*epggrab: (.+) - (.*)$')
-        re_unsub = re.compile(r'.*subscription: [0-9a-fA-F]{4}: "epggrab" unsubscribing')
+        with f as open(file, 'rt'):
+            for line in f.readline():
+                m_re_sub = re_sub.match(line)
+                m_re_epggrab = re_epggrab.match(line)
+                m_re_unsub = re_unsub.match(line)
 
-        for line in file:
-
-            m_re_sub = re_sub.match(line)
-            m_re_epggrab = re_epggrab.match(line)
-            m_re_unsub = re_unsub.match(line)
-
-            if m_re_sub:
-                current_mux = m_re_sub.group(1)
-                if not self.muxes.get(current_mux):
-                    self.muxes[current_mux] = {}
-            elif m_re_epggrab:
-                if current_mux is None:
-                    print('No subscription found for %s: %s' % (m_re_epggrab.group(1), line))
+                if m_re_sub:
+                    mux_name = m_re_sub.group(1)
+                    current_mux = EITMux(mux_name)
+                elif m_re_epggrab:
+                    if current_mux is None:
+                        print('No subscription found for %s: %s' % (m_re_epggrab.group(1), line))
+                    else:
+                        eit_name = m_re_epggrab.group(1)
+                        eit_result = m_re_epggrab.group(2)
+                        grabber = EITGrabber(eit_name, eit_result)
+                        current_mux.add_grabber(grabber)
+                elif m_re_unsub:
+                    current_mux = None
                 else:
-                    current_eit = m_re_epggrab.group(1)
-                    eit_result = m_re_epggrab.group(2)
-                    self.muxes[current_mux][current_eit] = eit_result
-            elif m_re_unsub:
-                current_mux = None
-            else:
-                next
+                    next
+    
+class EITGrabber:
 
+    def __init__(self, name, description):
+        self.name = name
+        self.description: description
+
+class EITMux:
+
+    def __init__(self, name):
+        self.name = name
+        self.grabbers = list()
+
+    def add_grabber(self, grabber):
+        self.grabbers.append(grabber)
+        
 # Demonstration and testing.
 #
-def demo():
-    import fileinput
+def main():
     import pprint
 
     muxes = EITParser()
-    muxes.parse(fileinput.input())
+    muxes.parse('/var/log/syslog')
     pprint.pprint(muxes.muxes)
 
 if __name__ == '__main__':
-    demo()
+    main()
